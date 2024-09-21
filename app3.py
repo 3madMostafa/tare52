ea6a8575a1653da60ff7f2e833124d9810bbfb8d
3madMostafa <118833050+3madMostafa@users.noreply.github.com>
2024-09-21 10:19:37 +0300

    Delete app3.py

--- app3.py
+++
@@ -1,4 +0,0 @@
-from transformers import GPT2LMHeadModel, GPT2Tokenizer
-
-model = GPT2LMHeadModel.from_pretrained("fine_tuned_aragpt2")
-tokenizer = GPT2Tokenizer.from_pretrained("fine_tuned_aragpt2")
